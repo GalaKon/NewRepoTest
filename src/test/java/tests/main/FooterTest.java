f984f748d0aef637b3b08ec13afd74ca6994ed7b
GalaKon <gala.kon13@gmail.com>
2022-07-27 18:06:17 +0400

    Exeptional Assert added

--- src/test/java/tests/main/FooterTest.java
+++ src/test/java/tests/main/FooterTest.java
@@ -2,31 +2,17 @@ package tests.main;
 
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.MainPage;
 import runner.BaseTest;
+import tests.TestData;
 
 import java.util.List;
 
 public class FooterTest extends BaseTest {
 
-    @DataProvider(name = "FooterTestData")
-    public Object[][] footerDataProvider() {
-
-        return new Object[][]{
-                {0, "Start", "http://www.99-bottles-of-beer.net/"},
-                {1, "Browse Languages", "http://www.99-bottles-of-beer.net/abc.html"},
-                {2, "Search Languages", "http://www.99-bottles-of-beer.net/search.html"},
-                {3, "Top Lists", "http://www.99-bottles-of-beer.net/toplist.html"},
-                {4, "Guestbook", "http://www.99-bottles-of-beer.net/guestbookv2.html"},
-                {5, "Submit new Language", "http://www.99-bottles-of-beer.net/submitnewlanguage.html"}
-        };
-    }
-
     @Test(
             priority = 1
-            //dataProvider = "FooterTestData"
     )
     public void testFootersCount() {
         int expectedFooterCount = 6;
@@ -39,10 +25,11 @@ public class FooterTest extends BaseTest {
     @Test(
             priority = 2,
             dependsOnMethods = "testFootersCount",
+            dataProviderClass = TestData.class,
             dataProvider = "FooterTestData"
     )
 
-    public void testFootersTextsAndLinks(int index, String footerName, String footerLink) {
+    public void testFootersTextsAndLinks(int index, String footerName, String footerLink, String footerTitle) {
 
         MainPage main = openBaseURL();
         List<WebElement> footers = main.getFooterAll();
@@ -51,30 +38,6 @@ public class FooterTest extends BaseTest {
 
         Assert.assertEquals(footerText, footerName);
         Assert.assertEquals(footerLinks.get(index), footerLink);
-    }
-
-    @Test(
-            priority = 2,
-            dependsOnMethods = {"testFootersCount", "testFootersTextsAndLinks"},
-            dataProvider = "FooterTestData"
-    )
-
-    public void testFootersNavigtionToURLAndReturnBack(int index, String footerName, String footerLink) {
-
-        MainPage main = openBaseURL();
-        String baseLink = getDriver().getCurrentUrl();
-
-        main.clickLink(index);
-        String newLink = getDriver().getCurrentUrl();
-
-        Assert.assertNotEquals(newLink, baseLink, "<<<New URL equals previous URL, probably we clicked on the same menu>>>");
-        Assert.assertEquals(newLink, footerLink);
-
-        getDriver().navigate().back();
-        String goBackLink = getDriver().getCurrentUrl();
-
-        Assert.assertNotEquals(newLink, goBackLink);
-        Assert.assertEquals(goBackLink, baseLink);
     }
 
     @Test(
@@ -87,40 +50,5 @@ public class FooterTest extends BaseTest {
         String actualFooterMenuBar = openBaseURL().getFooterBarText();
 
         Assert.assertEquals(actualFooterMenuBar, expectedFooterMenuBar);
-    }
-
-    @Test
-    public void testFooterBrowseLanguageButton() {
-        String expectedAbcPageUrl = "http://www.99-bottles-of-beer.net/abc.html";
-
-        String actualAbcPageUrl = openBaseURL()
-                .clickFooterBrowseLanguages()
-                .getDriver()
-                .getCurrentUrl();
-
-        Assert.assertEquals(actualAbcPageUrl, expectedAbcPageUrl);
-    }
-
-    @Test
-    public void testFooterSearchButton() {
-        String expectedSearchPageUrl = "http://www.99-bottles-of-beer.net/search.html";
-
-        String actualSearchPageUrl = openBaseURL()
-                .clickFooterSearchLanguages()
-                .getDriver()
-                .getCurrentUrl();
-
-        Assert.assertEquals(actualSearchPageUrl, expectedSearchPageUrl);
-    }
-
-    @Test
-    public void testFooterTopListButton() {
-        String expectedTopListPageUrl = "http://www.99-bottles-of-beer.net/toplist.html";
-
-        String actualTopListPageUrl = openBaseURL()
-                .clickFooterTopLists()
-                .getDriver()
-                .getCurrentUrl();
-        Assert.assertEquals(actualTopListPageUrl, expectedTopListPageUrl);
     }
 }
